fe1b918255b756c61605562bac32868a0312be57
romeroviviana <romerovivianapr@gmail.com>
2024-07-15 19:07:09 -0500

    class 8 - Firts API called

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,16 +1,16 @@
-const Card = () => {
+const Card = (data) => {
     return (
         <div className="bg-white cursor-pointer w-56 h-60 rounded-lg">
             <figure className="relative mb-2 w-full h-4/5">
-                <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-1">Electronics</span>
-                <img src="https://images.pexels.com/photos/1037992/pexels-photo-1037992.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="headphones" className=" w-full h-full object-cover rounded-lg" />
+                <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-1">{data.data.category.name}</span>
+                <img src={data.data.images[0]} alt={data.data.title} className=" w-full h-full object-cover rounded-lg" />
                 <div className=" absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1">
                 +
                 </div>
             </figure>
             <p className=" flex justify-between align-middle">
-                <span className="text-sm font-light">Headphones</span>
-                <span className="text-lg font-medium">$300</span>
+                <span className="text-sm font-light">{data.data.title}</span>
+                <span className="text-lg font-medium">${data.data.price}</span>
             </p>
         </div>
     )
